abb88b41a37355dcd692ed420c8b892da01c2252
大熊熊 <s86390474@yahoo.com>
2022-12-04 09:33:40 +0800

    yyy

---
+++ src/main/java/com/example/demo/YyyyyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyyyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YyyyyApplication.class, args);
+	}
+
+}
