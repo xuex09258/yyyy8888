a807347c5f1777146f0815b76a1f34310247d489
大熊熊 <s86390474@yahoo.com>
2023-02-12 05:21:23 +0800

    yyyy

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -17,44 +17,72 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// 配置身份驗證時一定要做的 1111 2:11
 	@Bean
 	PasswordEncoder password() {
-		return new BCryptPasswordEncoder(); // 將密碼進行加密(每次加密資料並不會相同) 強度可以輸入 4~31
+		return new BCryptPasswordEncoder(); // 2222 00:54將密碼進行加密(每次加密資料並不會相同) 強度可以輸入 4~31
 	}
 	// 配置身份驗證 1111 2:11
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//2222 0:43 加 log
+		System.out.println("配置身份驗證configure(AuthenticationManagerBuilder auth)");
+		//--------------------------------------------------------------------
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String password = passwordEncoder.encode("1234");
 		auth.inMemoryAuthentication()
 			.withUser("john")
 			.password(password)
-			.roles("ADMIN");
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(web);
+			//.roles("ADMIN");
+		    .roles("ADMIN", "USER");//2222 00:14
+		auth.inMemoryAuthentication()//2222 00:14
+			.withUser("mary")
+			.password(password)
+			.roles("USER");
 	}
 
 	// 配置 HTTP 安全性
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			//System.out.println("configure(HttpSecurity http)");
+			System.out.println("配置 HTTP 安全性configure(HttpSecurity http)");
+			//------------------------------------------------------------
 			http.authorizeHttpRequests() // 授權請求
-				// 設定放行名單
+				// 設定放行名單2222 0:17
+			.antMatchers("/admin").hasRole("ADMIN")  //只有限制 admin 葉面 只有ADMIN角色 能進來
+			//.antMatchers("/user").hasRole("USER")    //只有限制 user  葉面
+			.antMatchers("/user").hasAnyRole("USER", "ADMIN") //hasAnyRole("USER", "ADMIN") 有兩個腳色時
+			.anyRequest().permitAll() //-------------- 其他請求皆開放
+			.and().formLogin(); // 利用表單來登入
+	//---------------------------------------------------------
+//			    //2222 30分 
 //				//.antMatchers(HttpMethod.GET, "/admin").hasAuthority("ROLE_ADMIN") // ADMIN -> ROLE_ADMIN
-//				.antMatchers("/admin").hasRole("ADMIN")
+	//----------------------------------------------------------------
+			//2222 00:32 css images js  下面要 配置網路安全 2222 00:32
+			//.antMatchers("/admin").hasRole("ADMIN")
+			//.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+			//.anyRequest().authenticated() // *********所有請求都要驗證
+			//.and().formLogin(); // 利用表單來登入
+	//---------------------------------------------------------
+//			    .antMatchers("/admin").hasRole("ADMIN")
 //				.antMatchers("/user").hasAnyRole("USER", "ADMIN")
 //				.antMatchers("/jwt").authenticated()
 //				.anyRequest().permitAll() // 其他請求皆開放
-				.anyRequest().authenticated() // 所有請求都要驗證
-				.and().formLogin(); // 利用表單來登入
+//			    .and().formLogin(); // 利用表單來登入
+	//-------------------------------------------------------------
+		//初3的5 配置身份驗證1111 2:11 時 *所有請求都要驗證
+			//.anyRequest().authenticated() // *********所有請求都要驗證
+			//.and().formLogin(); // 利用表單來登入
 			
 			http.rememberMe() // 不會因為瀏覽器關閉而消失登入狀態
 				.tokenValiditySeconds(30)
 				.key("mykey");
 			
 			
+		}
+		// 配置網路安全 2222 00:32
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			
+			System.out.println("配置網路安全configure(WebSecurity web)");
+			// 不需要驗證的路徑-----------------------------------------------
+			web.ignoring().antMatchers("/css/**", "/images/**", "/js/**");
 		}
 
 }
